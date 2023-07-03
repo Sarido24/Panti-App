091bd3e5bbdf26e73afa36d838333e819adc7465
Sarido <sarido24@gmail.com>
2023-07-03 21:18:07 +0700

    Add: Landing Page

---
+++ panti-app/app/about/page.jsx
@@ -0,0 +1,5 @@
+export default function About(){
+    return (
+        <><h1 className="text-2xl my-16 font-bold text-center">About</h1></>
+      )
+}
---
+++ panti-app/app/gallery/page.jsx
@@ -0,0 +1,5 @@
+export default function Gallery(){
+    return (
+        <><h1 className="text-2xl my-16 font-bold text-center">Gallery</h1></>
+      )
+}
